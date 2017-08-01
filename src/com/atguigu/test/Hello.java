003b091cb4a7931c226e0096710449a5bb775ac1
kongyuan <kongyuan@163.com>
2017-08-01 11:51:16 +0800

    di 3 ci

--- src/com/atguigu/test/Hello.java
+++ src/com/atguigu/test/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		System.out.println("how are you");
+		System.out.println("this is 1990");
 	}
 }
